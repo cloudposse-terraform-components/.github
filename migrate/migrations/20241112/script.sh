3cce59075c4e6ed79dcd81a2de35fea70fe238dc
Igor Rodionov <goruha@gmail.com>
2024-12-12 18:01:44 +0100

    Added migration

--- migrate/migrations/20241112/script.sh
+++ migrate/migrations/20241112/script.sh
@@ -2,9 +2,12 @@ title "Fix readme"
 
 if [ -f CHANGELOG.md ]; then
 	mv CHANGELOG.md ./src
-	git add ./src/CHANGELOG.md
+	git add src/CHANGELOG.md
 fi
 
+make init
+make readme/build README_TEMPLATE_REPO_PATH=README.md.simple.gotmpl README_FILE=src/README.md
+git add src/README.md
 
 # Merge the PR
 # auto_merge
